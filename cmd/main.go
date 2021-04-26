a1d2715b84ac80f8470cb93d02edb5a66a5b62ff
chaitradech <kullachandachaitra@gmail.com>
2021-04-26 18:20:15 +0530

    added testsuit

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	a := Fraction{Numerator: 6, Denominator: 10}
-	b := Fraction{Numerator: 4,Denominator: 10}
+	b := Fraction{Numerator: 4, Denominator: 10}
 	fmt.Println(MultiplyFractions(a, b))
 }
--- fractions/multiply_fractions_test.go
+++ fractions/multiply_fractions_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-//Testing ComputeGCD function
+//TestComputeGCD tests the functionality of ComputeGCD.
 
 func TestComputeGCD(t *testing.T) {
 	got := ComputeGCD(6, 10)
@@ -19,7 +19,7 @@ func assertEqual(t *testing.T, got int64, expected int64) {
 	}
 }
 
-//Testing with inputs a=1/2 and b=1/3, expected o/p res=1/6
+//TestMultiplyFractions1 inputs a=1/2 and b=1/3, and expectes o/p res=1/6.
 func TestMultiplyFractions1(t *testing.T) {
 	a := Fraction{Numerator: 1, Denominator: 2}
 	b := Fraction{Numerator: 1, Denominator: 3}
@@ -33,7 +33,7 @@ func TestMultiplyFractions1(t *testing.T) {
 	}
 }
 
-//Testing with inputs a=6/10 and b=4/10, expected o/p res=6/25
+//TestMultiplyFractions2 inputs a=6/10 and b=4/10, and expectes o/p res=6/25.
 func TestMultiplyFractions2(t *testing.T) {
 	a := Fraction{Numerator: 6, Denominator: 10}
 	b := Fraction{Numerator: 4, Denominator: 10}
@@ -47,7 +47,7 @@ func TestMultiplyFractions2(t *testing.T) {
 	}
 }
 
-//Testing with inputs a=6/5 and b=4/10, expected o/p res=12/25
+//TestMultiplyFractions3 inputs a=6/5 and b=4/10, and expectes o/p res=12/25.
 func TestMultiplyFractions3(t *testing.T) {
 	a := Fraction{Numerator: 6, Denominator: 5}
 	b := Fraction{Numerator: 4, Denominator: 10}
@@ -61,7 +61,7 @@ func TestMultiplyFractions3(t *testing.T) {
 	}
 }
 
-//Testing with inputs a=12/10 and b=10/12, expected o/p : 1/1
+//TestMultiplyFractions4 inputs a=12/10 and b=10/12, and expectes o/p : 1/1.
 func TestMultiplyFractions4(t *testing.T) {
 	a := Fraction{Numerator: 12, Denominator: 10}
 	b := Fraction{Numerator: 10, Denominator: 12}
@@ -75,7 +75,7 @@ func TestMultiplyFractions4(t *testing.T) {
 	}
 }
 
-//Testing with inputs a=3/6 and b=5/9, expected o/p res=5/18
+//TestMultiplyFractions5 inputs a=3/6 and b=5/9, and expectes o/p res=5/18.
 func TestMultiplyFractions5(t *testing.T) {
 	a := Fraction{Numerator: 3, Denominator: 6}
 	b := Fraction{Numerator: 5, Denominator: 9}
@@ -89,7 +89,7 @@ func TestMultiplyFractions5(t *testing.T) {
 	}
 }
 
-//Testing with inputs a=7/3 and b=5/10, expected o/p res=21/50
+//TestMultiplyFractions6 inputs a=7/3 and b=5/10, and expectes o/p res=21/50.
 func TestMultiplyFractions6(t *testing.T) {
 	a := Fraction{Numerator: 7, Denominator: 5}
 	b := Fraction{Numerator: 3, Denominator: 10}
@@ -103,7 +103,7 @@ func TestMultiplyFractions6(t *testing.T) {
 	}
 }
 
-//Testing with inputs a=89/10 and b=14/17, expected o/p res=623/85
+//TestMultiplyFractions7 inputs a=89/10 and b=14/17, and expectes o/p res=623/85.
 func TestMultiplyFractions7(t *testing.T) {
 	a := Fraction{Numerator: 89, Denominator: 10}
 	b := Fraction{Numerator: 14, Denominator: 17}
@@ -117,7 +117,7 @@ func TestMultiplyFractions7(t *testing.T) {
 	}
 }
 
-//Testing with inputs a=22/11 and b=1/3, expected o/p res=2/3
+//TestMultiplyFractions8 inputs a=22/11 and b=1/3, and expectes o/p res=2/3.
 func TestMultiplyFractions8(t *testing.T) {
 	a := Fraction{Numerator: 22, Denominator: 11}
 	b := Fraction{Numerator: 1, Denominator: 3}
@@ -131,7 +131,7 @@ func TestMultiplyFractions8(t *testing.T) {
 	}
 }
 
-//Testing with inputs a=55/23 and b=21/18, expected o/p res=1265/378
+//TestMultiplyFractions9 inputs a=55/23 and b=21/18, and expectes o/p res=1265/378.
 func TestMultiplyFractions9(t *testing.T) {
 	a := Fraction{Numerator: 55, Denominator: 21}
 	b := Fraction{Numerator: 23, Denominator: 18}
@@ -145,7 +145,7 @@ func TestMultiplyFractions9(t *testing.T) {
 	}
 }
 
-//Testing with inputs a=19/17 and b=13/47, expected o/p res=323/611
+//TestMultiplyFractions10 inputs a=19/17 and b=13/47, and expectes o/p res=323/611.
 func TestMultiplyFractions10(t *testing.T) {
 	a := Fraction{Numerator: 19, Denominator: 13}
 	b := Fraction{Numerator: 17, Denominator: 47}
@@ -156,5 +156,33 @@ func TestMultiplyFractions10(t *testing.T) {
 		return
 	} else {
 		t.Errorf("Didn't match expected result for MultiplyFractions(%d,%d) = %d ;", a, b, res)
+	}
+}
+
+//TestMultiplyFractionsTogether tests multiple inputs one after other for optimisation.
+func TestMultiplyFractionsTogether(t *testing.T) {
+	var tests = []struct {
+		a      Fraction
+		b      Fraction
+		result Fraction
+	}{
+		{Fraction{7, 3}, Fraction{5, 10}, Fraction{21, 50}},
+		{Fraction{89, 10}, Fraction{14, 17}, Fraction{623, 85}},
+		{Fraction{55, 23}, Fraction{21, 18}, Fraction{1265, 378}},
+		{Fraction{19, 17}, Fraction{13, 47}, Fraction{323, 611}},
+		{Fraction{1, 2}, Fraction{1, 2}, Fraction{1, 2}},
+		{Fraction{1, 2}, Fraction{1, 3}, Fraction{1, 6}},
+		{Fraction{6, 10}, Fraction{4, 10}, Fraction{6, 25}},
+		{Fraction{6, 5}, Fraction{4, 10}, Fraction{12, 25}},
+		{Fraction{3, 6}, Fraction{5, 9}, Fraction{5, 18}},
+	}
+
+	for _, test := range tests {
+		total := MultiplyFractions(test.a, test.b)
+		if total.Numerator != test.result.Numerator && total.Denominator != test.result.Denominator {
+			return
+		} else {
+			t.Errorf("Didn't match expected result for MultiplyFractions(%d,%d) = Got: %d , Expected: %d;", test.a, test.b, test.result, total)
+		}
 	}
 }
