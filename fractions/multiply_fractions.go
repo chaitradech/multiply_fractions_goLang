76b109971ce8fde25317c35b06bea4a35d18924b
chaitradech <kullachandachaitra@gmail.com>
2021-04-26 17:05:55 +0530

    format code

--- fractions/multiply_fractions.go
+++ fractions/multiply_fractions.go
@@ -1,34 +1,33 @@
 package fractions
 
-
-// To be treated as a class, to represent fractions
+// Fractions signifies a fraction value.
 type Fraction struct {
-	Numerator, Denominator int
+	Numerator   int64
+	Denominator int64
 }
 
-//Function to convert each of the fraction on to it's simplified form
-func Reduce(input Fraction) (res1, res2 int) {
-	gcd := ComputeGCD(input.Numerator,input.Denominator)
-	res1 = input.Numerator/ gcd
-	res2 = input.Denominator/ gcd
+//Reduce simplifies each of the fraction value on to it's reduce form.
+func reduce(input Fraction) (res1, res2 int64) {
+	gcd := ComputeGCD(input.Numerator, input.Denominator)
+	res1 = input.Numerator / gcd
+	res2 = input.Denominator / gcd
 	return
 }
 
-//Function to calculate the GCD 
-func ComputeGCD( numerator ,denominator int) int {
-	if (denominator == 0) {
+//ComputeGCD returns the GCD for the fraction values.
+func ComputeGCD(numerator, denominator int64) int64 {
+	if denominator == 0 {
 		return numerator
 	}
-	return ComputeGCD(denominator, numerator % denominator)
+	return ComputeGCD(denominator, numerator%denominator)
 }
 
-//Function to Multiply the simplified inputs
-func MultiplyFractions( input1 Fraction, input2 Fraction) Fraction {
-	output1 := input1.Numerator *  input2.Numerator
+//MultiplyFractions performs multiplication of two fraction values.
+func MultiplyFractions(input1 Fraction, input2 Fraction) Fraction {
+	output1 := input1.Numerator * input2.Numerator
 	output2 := input1.Denominator * input2.Denominator
-	
-	ans := Fraction{Numerator: output1 , Denominator: output2}
-	ans.Numerator, ans.Denominator = Reduce(ans)
-	return ans 
-}
 
+	ans := Fraction{Numerator: output1, Denominator: output2}
+	ans.Numerator, ans.Denominator = reduce(ans)
+	return ans
+}
